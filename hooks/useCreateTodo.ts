af87fde18bd98f8c43d64e1a98ecf4bed30168d6
Glendon Philipp Baculio <gpbaculio@gmail.com>
2023-11-04 02:32:19 +0800

    Update useCreateTodo.ts

--- hooks/useCreateTodo.ts
+++ hooks/useCreateTodo.ts
@@ -5,12 +5,10 @@ import {supabase} from 'supabase';
 import {TODOS_QUERY_KEY, Todo} from './useGetTodos';
 import {PostgrestSingleResponse} from '@supabase/supabase-js';
 
-const useCreateTodo = () => {
-  return useMutation<PostgrestSingleResponse<Todo[]>, Error, string, unknown>({
-    mutationFn: async (text: string) => {
-      return await supabase.from(TODOS_QUERY_KEY).insert([{text}]).select();
-    },
+const useCreateTodo = () =>
+  useMutation<PostgrestSingleResponse<Todo[]>, Error, string, unknown>({
+    mutationFn: async (text: string) =>
+      await supabase.from(TODOS_QUERY_KEY).insert([{text}]).select(),
   });
-};
 
 export default useCreateTodo;
